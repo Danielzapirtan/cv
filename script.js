494df99a14d5a55f92a15bd4f681a5c4970d7388
Danielzapirtan <antoniudanielzapirtan@gmail.com>
2026-09-05 16:55:55 +0300

    defaultMessage

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ date.min = new Date().toISOString().slice(0,10);
 const formular = document.getElementById("form");
 const aplica = document.getElementById("aplica");
 const table = document.getElementById("table");
+localStorage.removeItem("db");
 const getdb = localStorage.getItem("db");
 if (getdb) {
   table.innerHTML = JSON.parse(getdb);
